877d9acecdc30a3ebfd779ddc48f34d087487c89
SimaDovakin <simadovakin999@gmail.com>
2021-04-12 16:06:28 +0300

    Customized admin page

--- myfirstsite/news/views.py
+++ myfirstsite/news/views.py
@@ -7,8 +7,9 @@ from .models import News
 def index(request):
     return HttpResponse('<h1>Hello, world!</h1>')
 
-def news(request):
-    news = News.objects.all()
-    title = 'News List'
 
-    return render(request, 'news/news_list.html', context={'news': news, 'title': title})
+def news(request):
+    news_list = News.objects.order_by('-created_at')
+    context = {'title': 'News List', 'news': news_list}
+
+    return render(request, 'news/news_list.html', context=context)
