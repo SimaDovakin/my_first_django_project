983ce23a7ba03c3ed255d4f3723d330a1e88bca1
SimaDovakin <simadovakin999@gmail.com>
2021-04-14 11:31:37 +0300

    Added category to the editable list of news

--- myfirstsite/news/admin.py
+++ myfirstsite/news/admin.py
@@ -6,7 +6,7 @@ from .models import News, Category
 class NewsAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'category', 'created_at', 'updated_at', 'is_published')
     list_display_links = ('id', 'title')
-    list_editable = ('is_published',)
+    list_editable = ('is_published', 'category')
     search_fields = ('title', 'content')
     list_filter = ('is_published', 'category')
 
@@ -15,6 +15,7 @@ class CategoryAdmin(admin.ModelAdmin):
     list_display = ('id', 'title')
     list_display_links = ('id', 'title')
     search_fields = ('title', )
+
 
 admin.site.register(News, NewsAdmin)
 admin.site.register(Category, CategoryAdmin)
