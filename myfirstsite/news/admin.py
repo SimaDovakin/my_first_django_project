7ffd82eaad25b4adb8260c587aa21fc0dc16ace7
Ruslan Simchuk <simadovakin999@gmail.com>
2021-05-02 15:05:10 +0300

    Customize admin panel

--- myfirstsite/news/admin.py
+++ myfirstsite/news/admin.py
@@ -1,14 +1,30 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
 from .models import News, Category
 
 
 class NewsAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'category', 'created_at', 'updated_at', 'is_published', 'views')
+    save_on_top = True
+
+    list_display = ('id', 'title', 'category',
+                    'created_at', 'updated_at',
+                    'is_published', 'views', 'get_photo')
     list_display_links = ('id', 'title')
     list_editable = ('is_published', 'category')
     search_fields = ('title', 'content')
     list_filter = ('is_published', 'category')
+    fields = ('title', 'category', 'content',
+              'photo', 'get_photo', 'is_published',
+              'created_at', 'updated_at', 'views')
+    readonly_fields = ('get_photo', 'created_at', 'updated_at', 'views')
+
+    def get_photo(self, obj):
+        if obj.photo:
+            return mark_safe(f'<img src="{obj.photo.url}" width="60">')
+        return "Фото отсутствует"
+
+    get_photo.short_description = "Миниатюра"
 
 
 class CategoryAdmin(admin.ModelAdmin):
@@ -19,3 +35,6 @@ class CategoryAdmin(admin.ModelAdmin):
 
 admin.site.register(News, NewsAdmin)
 admin.site.register(Category, CategoryAdmin)
+
+admin.site.site_title = 'Админка "Обучающий проект"'
+admin.site.site_header = 'Админка "Обучающий проект"'
