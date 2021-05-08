c2b3484fe319e3ba52d1e1468c98123707a76b61
Ruslan Simchuk <simadovakin999@gmail.com>
2021-05-08 15:55:55 +0300

    Edit pagination

--- myfirstsite/news/views.py
+++ myfirstsite/news/views.py
@@ -42,7 +42,10 @@ class NewsByCategory(ListView):
     paginate_by = 2
 
     def get_queryset(self):
-        return News.objects.filter(is_published=True, category_id=self.kwargs['category_id']).select_related('category')
+        return News.objects.filter(
+            is_published=True,
+            category_id=self.kwargs['category_id']
+        ).select_related('category')
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
